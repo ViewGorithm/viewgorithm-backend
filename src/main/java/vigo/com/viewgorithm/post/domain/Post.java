fa332f84e09c284781d0c5c5fcb26664f49bf5f3
Hyun jinyong <ekwls20@gmail.com>
2024-06-14 15:07:13 +0900

    Post 도메인 연관관계 삭제

--- src/main/java/vigo/com/viewgorithm/post/domain/Post.java
+++ src/main/java/vigo/com/viewgorithm/post/domain/Post.java
@@ -1,8 +1,8 @@
 package vigo.com.viewgorithm.post.domain;
+
 import jakarta.persistence.*;
 import lombok.*;
-import vigo.com.viewgorithm.member.domain.Member;
-import java.time.LocalDateTime;
+
 import java.util.Date;
 @Getter
 @Setter
@@ -17,16 +17,11 @@ public class Post{
 
     @Column(columnDefinition = "VARCHAR(100)")
     private String title;
-
+    
     @Column(columnDefinition = "TEXT")
     private String content;
 
     @Column(name = "created_at")
     @Temporal(TemporalType.TIMESTAMP)
     private Date created_at;
-
-    @ManyToOne // 게시글 다 : 유저 1
-    @JoinColumn(name = "member_id", referencedColumnName = "id")
-    private Member member;
-
 }
